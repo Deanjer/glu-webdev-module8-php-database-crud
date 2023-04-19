5bbc86a172e990175e9957d8fa9b11bd22ed7a9b
Deanjer <220509@student.glu.nl>
2023-04-19 11:21:15 +0200

    database_crud

---
+++ database.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/database.css">
+</head>
+
+<body>
+    <?php include 'connect.php';
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['save'])) {
+        
+        include 'connect.php';
+    
+        $product = $_POST['product'];
+        $price = $_POST['price'];
+        $img = $_POST['img'];
+    
+        $stmt = $conn->prepare("INSERT INTO productdata (product, price, img) VALUES (:product, :price, :img)");
+    
+        $stmt->bindParam(':product', $product);
+        $stmt->bindParam(':price', $price);
+        $stmt->bindParam(':img', $img);
+    
+        
+        if ($stmt->execute()) {
+            header('Location: ' . $_SERVER['PHP_SELF']);
+            exit();
+        } else {
+            echo "Error: " . $stmt->errorInfo()[2];
+        }
+    }
+    ?>
+    <div class="main-wrapper">
+    <div class="crud-wrapper">
+        <form action="" method="POST">
+            <div class="database-inssert margin-database">
+                <label>Product</label>
+                <input type="text" name="product" value="">
+            </div><br>
+            <div class="database-inssert">
+                <label>Prijs</label>
+                <input type="text" name="price" value="">
+            </div><br>
+            <div class="database-inssert">
+                <label>Img</label>
+                <input type="text" name="img" value="">
+            </div><br>
+            <div class="database-inssert"><br>
+                <button class="btn" type="submit" name="save">Opslaan</button>
+            </div>
+        </form>
+    </div>
+    
+    
+        <div class="table-wrapper">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Product</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Img</th>
+                        <!-- <th scope="col">Handle</th> -->
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    try {
+                        $stmt = $conn->query("SELECT id, product, price, img FROM productdata");
+                        
+                        while ($row = $stmt->fetch()) {
+                           
+                        
+                   
+                    
+                ?>
+                    <tr>
+                        <td><?php echo $row['id']; ?></td>
+                        <td><?php echo $row['product']; ?></td>
+                        <td><?php echo '€'.$row['price']; ?></td>
+                        <td><?php echo '<img class="img-db" src="'.$row['img'].'">'; ?></td>
+                    </tr>
+                    <?php  } } catch(PDOException $e) {
+                        echo "Error: " . $e->getMessage(); 
+                        } ?>
+                </tbody>
+            </table>
+
+        </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- detail.php
+++ detail.php
@@ -1,45 +1,96 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <style>
-        *{
-            margin: 0px;
-            paddin: 0px;
-        }
-        .detail-main{
-            background-color: rgb(208, 206, 206);
-            width: 100%;
-            height: 60vh;
-        }
-        .detail-wrapper{
-            display: flex;
-            text-align: center;
-            justify-content: center;
-        }
-        img{
-            width: 40%;
-            max-heigth: 250px
-        }
-        </style>
-        <div class="detail-main">
-<div class="detail-wrapper">
-<?php $productId = $_GET['id'];
+    * {
+        margin: 0px;
+        paddin: 0px;
+    }
+
+    .detail-main {
+        background-color: rgb(239, 236, 236);
+        width: 100%;
+        height: auto;
+    }
+
+    .detail-wrapper {
+        display: flex;
+        text-align: center;
+        justify-content: center;
+    }
+
+    img {
+        width: 40%;
+        max-heigth: 250px
+    }
+
+    .product-view {
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
+
+    h1 {
+        color: rgb(145, 144, 144);
+        font-size: 50px;
+    }
+
+    p {
+        color: rgb(145, 144, 144);
+        font-size: 25px;
+    }
+
+    .main-menu {
+        width: 100%;
+        height: 60px;
+        background-color: rgb(239, 236, 236);
+        display: flex;
+        justify-content: space-around;
+    }
+    .main-menu-wrapper{
+        margin-top: 1rem;
+    }
+    .main-menu-wrapper button{
+        width: 100px;
+        height: 30px;
+        border: none;
+        background-color: #B9F5D8;
+        border-radius: 10px;
+        color: white;
+    }
+    </style>
+    <div class="main-menu">
+        <div class="main-menu-wrapper">
+        <div>
+            <a href="webshop.php"><button>Home</button></a>
+        </div>
+        </div>
+        <div></div>
+    </div>
+    <div class="detail-main">
+        <div class="detail-wrapper">
+            <?php $productId = $_GET['id'];
                 include 'connect.php';
 
                 try {
                     $stmt = $conn->query("SELECT id, product, price, img FROM productdata WHERE id = $productId");
                     
                     while ($row = $stmt->fetch()) {
-                        echo '<div class="product-card">';
+                        echo '<div class="product-view">';
                         echo '<img class="img" src="'. $row['img'].' "> <br>';
-                        echo $row['product'].'<br> €'.$row['price']. '<br>' . "\n";
-                        echo '</div></a>';
+                        echo '<div class="product-beschrijving">';
+                        echo '<h1>'.$row['product'].'</h1>'.'<br> <p>€'.$row['price']. '</p><br>';
+                        echo '</div></div></a>';
                     }
                 } catch(PDOException $e) {
                     echo "Error: " . $e->getMessage();
@@ -48,7 +99,8 @@
 
 
 ?>
-</div>
-</div>
+        </div>
+    </div>
 </body>
+
 </html>
